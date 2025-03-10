87dead000e3d067b1e46bd364e4ec25df9dae0f3
Fl0wyn <feramusflorian@icloud.com>
2025-03-10 20:03:45 +0100

    feat: Supprimer plusieurs fichiers d'icônes et d'images obsolètes

--- docs/components/Tools.vue
+++ docs/components/Tools.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { computed, ref } from "vue";
 import { data } from "../data";
 
@@ -26,36 +26,21 @@ const filteredData = computed(() => {
 
 <template>
   <div class="DocSearch DocSearch-Button" style="margin-top: 0.4rem">
-    <input
-      v-model="searchQuery"
-      type="search"
-      placeholder="Filter tools..."
-      style="width: 100%"
-    />
+    <input v-model="searchQuery" type="search" placeholder="Filter tools..." style="width: 100%" />
   </div>
 
   <div v-for="item in filteredData" :key="item.title" class="item">
     <h2 :id="item.title" tabindex="-1">
       {{ item.title }}
-      <a
-        class="header-anchor"
-        :href="'#' + item.title"
-        :aria-label="'Permalink to &quot;' + item.title + '&quot;'"
-        >​</a
-      >
+      <a class="header-anchor" :href="'#' + item.title"
+        :aria-label="'Permalink to &quot;' + item.title + '&quot;'"></a>
       <Badge type="tip" :text="item.items.length" style="margin-left: 0.2rem" />
     </h2>
     <div class="cards">
-      <a
-        v-for="app in item.items"
-        :key="app.app"
-        :href="app.url"
-        target="_blank"
-        class="app"
-      >
+      <a v-for="app in item.items" :key="app.app" :href="app.url" target="_blank" class="app">
         <div class="app-header">
           <span>{{ app.app }}</span>
-          <img :src="`/assets/${app.img}`" :alt="app.app" width="24" />
+          <img :src="`/images/${app.img}`" :alt="app.app" width="24" loading="lazy" />
         </div>
         <span class="detail">{{ app.detail }}</span>
       </a>
